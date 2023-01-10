fbf30e095a5cfae202df2db0a9ff83e8ebe08ebb
BioRaccoon <1190785@isep.ipp.pt>
2023-01-10 00:33:17 +0000

    Pause Menu Layout
    Corrections in the layout.

--- Assets/Scripts/TriggerDoorController.cs
+++ Assets/Scripts/TriggerDoorController.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class TriggerDoorController : MonoBehaviour
 {
     [SerializeField] private Animator myDoor = null;
+    [SerializeField] private bool foward = false;
+    [SerializeField] private bool backward = false;
+    [SerializeField] private GameObject fowardCollider = null;
+    [SerializeField] private GameObject backwardCollider = null;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-          myDoor.Play("DoorOpenFoward", 0, 0.0f);
-          gameObject.SetActive(false);
+            if (foward)
+            {
+                myDoor.Play("DoorOpenFoward", 0, 0.0f);
+                gameObject.SetActive(false);
+                Destroy(fowardCollider);
+                Destroy(backwardCollider);
+            }
+            else if (backward)
+            {
+                myDoor.Play("DoorOpenBackward", 0, 0.0f);
+                gameObject.SetActive(false);
+                Destroy(fowardCollider);
+                Destroy(backwardCollider);
+            }
         }
     }
 
