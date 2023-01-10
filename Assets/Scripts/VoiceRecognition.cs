1f0b7867b87424ea1a6fb9e698ebc19f48aa4239
BioRaccoon <1190785@isep.ipp.pt>
2023-01-10 00:01:14 +0000

    Room update
    Added Black Room.

--- Assets/Scripts/VoiceRecognition.cs
+++ Assets/Scripts/VoiceRecognition.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Windows.Speech;
 
 public class VoiceRecognition : MonoBehaviour
@@ -9,6 +10,7 @@ public class VoiceRecognition : MonoBehaviour
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, Action> stringToAction = new Dictionary<string, Action>();
     private Vector3 newPosition;
+    private bool keyword = false;
     void Start()
     {
         newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
@@ -27,6 +29,8 @@ public class VoiceRecognition : MonoBehaviour
     private void RecognizedSpeech(PhraseRecognizedEventArgs recognizedKeyword)
     {
         Debug.Log(recognizedKeyword.text);
+        InputSystem.DisableDevice(Keyboard.current);
+        keyword = true;
         stringToAction[recognizedKeyword.text].Invoke();
     }
 
@@ -50,13 +54,18 @@ public class VoiceRecognition : MonoBehaviour
 
     private void Update()
     {
-        if ((transform.position.x != newPosition.x) && (transform.position.z != newPosition.z))
+        //if ((transform.position.x != newPosition.x) && (transform.position.z != newPosition.z))
+        if ((transform.position.x != newPosition.x) && (transform.position.z != newPosition.z) && keyword)
         {
             Debug.Log(newPosition.y);
             newPosition.y = 0.6f;
             transform.position = Vector3.MoveTowards(transform.position, newPosition, 7.0f * Time.deltaTime);
         }
-            
-         
+
+        if (Vector3.Distance(transform.position, newPosition) < 0.1f)
+        {
+            keyword = false;
+            InputSystem.EnableDevice(Keyboard.current);
+        }
     }
 }
